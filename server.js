2256546621a140eb892db2b1c1d82948de4583da
KhaPham <Pham Hoang Kha>
2023-04-12 10:51:06 +0700

    add cors

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const path = require('path');
 const router = require('./api/routes/index')
+const cors = require('cors')
 require('dotenv').config()
 
 
@@ -15,6 +16,7 @@ const app = express();
 //Express urlencode;
 app.use(express.json())
 app.use(express.urlencoded({extended: false}));
+app.use(cors())
 
 //Express session
 app.use(require("express-session")({secret: process.env.SESSION_SECRECT_KEY||"thisismysecrctekeyfhrgfgrfrty84fwir767"}))
