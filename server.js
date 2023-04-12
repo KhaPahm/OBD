04a8755cd31077de78929f74f012fd0018f2142e
KhaPham <Pham Hoang Kha>
2023-04-12 21:51:59 +0700

    fix cors

--- server.js
+++ server.js
@@ -14,9 +14,14 @@ const port = process.env.PROT || 3000;
 const app = express();
 
 //Express urlencode;
-app.use(express.json())
+app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use(cors())
+app.use(cors());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
 
 //Express session
 app.use(require("express-session")({secret: process.env.SESSION_SECRECT_KEY||"thisismysecrctekeyfhrgfgrfrty84fwir767"}))
